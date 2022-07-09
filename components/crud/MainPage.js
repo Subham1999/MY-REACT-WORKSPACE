cf23a53d6ac7dfae5f42b32242e48725e09213bf
Subham1999 <subhamsantra2016@gmail.com>
2022-07-09 21:06:06 +0530

    StudentList

--- components/crud/MainPage.js
+++ components/crud/MainPage.js
@@ -10,7 +10,66 @@ const DepartmentList = [
   'Chemical Engineering',
 ];
 
+/**
+ * Drawback of this approach is it re-renders all the student elements 
+ * When there is any input change
+ */
+const StudentElement = ({ name, dept }) => {
+  console.log('Rendering -- ', name);
+  return (
+    <div className="card shadow">
+      <div className="card-body justify-content-between">
+        <span className="d-flex">{name}</span>
+        <span className="d-flex">{dept}</span>
+      </div>
+    </div>
+  );
+};
+
 class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      dept: '',
+      students: [],
+    };
+  }
+
+  onNameChange = (event) => {
+    this.setState({ name: event.target.value }, () =>
+      console.log(this.state.name)
+    );
+  };
+
+  onDeptChange = (event) => {
+    this.setState({ dept: event.target.value }, () =>
+      console.log(this.state.dept)
+    );
+  };
+
+  onSubmitForm = (event) => {
+    const { name, dept } = this.state;
+    if (name === null || name === '') {
+      alert('Name can not be empty');
+      return;
+    }
+    if (dept === null || dept === '' || dept === DepartmentList[0]) {
+      alert('Select a department');
+      return;
+    }
+
+    const newStudent = { name: name, dept: dept };
+    const newStudentList = this.state.students;
+    newStudentList.push(newStudent);
+    this.setState({ name: '', dept: '', students: newStudentList }, () => {
+      console.log(this.state);
+    });
+  };
+
+  generateKey = () => Math.round(Math.random() * 10000000);
+
   render() {
     const DepartmentOptionList = DepartmentList.map((department) => {
       if (department === 'Select Department') {
@@ -24,25 +83,49 @@ class MainPage extends React.Component {
       }
     });
 
+    const StudentListElement = this.state.students.map((student) => (
+      <StudentElement
+        name={student.name}
+        dept={student.dept}
+        key={this.generateKey}
+      />
+    ));
+
     return (
-      <div className="card shadow">
-        <div className="card-title text-center">
-          <b>Student Enrollment Form</b>
-        </div>
-        <div className="card-body">
-          <div className="form">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter name"
-            />
-            <select name="department" id="department" className="form-control">
-              <optgroup>{DepartmentOptionList}</optgroup>
-            </select>
-            <br />
-            <button className="btn btn-outline-success">submit</button>
+      <div>
+        <div className="card shadow">
+          <div className="card-title text-center">
+            <b>Student Enrollment Form</b>
+          </div>
+          <div className="card-body">
+            <div className="form">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter name"
+                value={this.state.name}
+                onChange={this.onNameChange}
+              />
+              <select
+                name="department"
+                id="department"
+                className="form-control"
+                value={this.state.dept}
+                onChange={this.onDeptChange}
+              >
+                <optgroup>{DepartmentOptionList}</optgroup>
+              </select>
+              <br />
+              <button
+                className="btn btn-outline-success"
+                onClick={this.onSubmitForm}
+              >
+                submit
+              </button>
+            </div>
           </div>
         </div>
+        <div className="studentList">{StudentListElement}</div>
       </div>
     );
   }
