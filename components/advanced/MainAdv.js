3ed0f2ac74f2b55505c5e3be7daa54af75ce1c07
subham-santra <32807547+Subham1999@users.noreply.github.com>
2022-07-10 09:53:23 +0530

    Create MainAdv.js

---
+++ components/advanced/MainAdv.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import NormalNameComponent from './NormalNameComponent';
+import PureNameComponent from './PureNameComponent';
+
+class MainAdv extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      someOtherValue: 100,
+      nameList: ['Subham', 'Nilimesh', 'Dolan', 'Ram', 'Shyam'],
+    };
+
+    this.incrementSomeOtherValueByOne =
+      this.incrementSomeOtherValueByOne.bind(this);
+  }
+
+  incrementSomeOtherValueByOne() {
+    const { someOtherValue } = this.state;
+    this.setState({
+      someOtherValue: someOtherValue + 1,
+    });
+  }
+
+  componentDidMount() {
+    console.log('******** MainAdv MOUNTED');
+  }
+
+  componentDidUpdate() {
+    console.log('####### MainAdv updated');
+  }
+
+  render() {
+    console.log('%%%%%%%% MainAdv rendered');
+    const NormalNameList = this.state.nameList?.map((name) => (
+      // <NormalNameComponent name={name} />
+      <PureNameComponent name={name} />
+    ));
+
+    return (
+      <div>
+        <p>Current Count : {this.state.someOtherValue}</p>
+        <button onClick={this.incrementSomeOtherValueByOne}>click me</button>
+        <br />
+        <div>{NormalNameList}</div>
+      </div>
+    );
+  }
+}
+
+export default MainAdv;
